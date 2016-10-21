5bb8a3f3666d37a62b3ea42e8075a7fe75a80add
yoshida_makoto <yoshida.makoto@adways.net>
2016-10-21 21:36:49 +0900

    キー変えられるようになった！！！！！！

--- app/src/main/java/com/example/yoshida_makoto/kotlintest/PlayerActivity.kt
+++ app/src/main/java/com/example/yoshida_makoto/kotlintest/PlayerActivity.kt
@@ -17,11 +17,11 @@ import com.google.android.exoplayer.upstream.DefaultAllocator
 import com.google.android.exoplayer.upstream.DefaultUriDataSource
 
 class PlayerActivity : AppCompatActivity(), ExoPlayer.Listener {
-
-    var player: ExoPlayer? = null;
-
     private val BUFFER_SEGMENT_SIZE = 64 * 1024
     private val BUFFER_SEGMENT_COUNT = 256
+    var audioRenderer: MediaCodecAudioTrackRenderer? = null
+    var exoPlayer: ExoPlayer? = null
+    var pitch: Float = 0.0f
 
     fun createIntent(context: Context, songId: Long): Intent {
         intent = Intent(context, PlayerActivity::class.java)
@@ -32,36 +32,46 @@ class PlayerActivity : AppCompatActivity(), ExoPlayer.Listener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.player_activity)
+
+
         val songId = intent.getLongExtra("song_id", 0)
-
-        val binding = DataBindingUtil.setContentView<PlayerActivityBinding>(this, R.layout.player_activity);
-
         val allocator = DefaultAllocator(BUFFER_SEGMENT_SIZE)
-
         val trackUri = ContentUris.withAppendedId(
                 MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, songId)
 
         val dataSource = DefaultUriDataSource(this, "userAgent")
+
         val sampleSource = ExtractorSampleSource(trackUri, dataSource, allocator,
                 BUFFER_SEGMENT_COUNT * BUFFER_SEGMENT_SIZE)
         val videoRenderer = MediaCodecVideoTrackRenderer(this, sampleSource,
                 MediaCodecSelector.DEFAULT, MediaCodec.VIDEO_SCALING_MODE_SCALE_TO_FIT)
 
-        val audioRenderer = MediaCodecAudioTrackRenderer(sampleSource, MediaCodecSelector.DEFAULT)
+        audioRenderer = MediaCodecAudioTrackRenderer(sampleSource, MediaCodecSelector.DEFAULT)
 
-        val rendererArray = arrayOf<TrackRenderer>(videoRenderer, audioRenderer)
+        val rendererArray = arrayOf<TrackRenderer>(videoRenderer, audioRenderer!!)
 
-        val exoPlayer = ExoPlayer.Factory.newInstance(rendererArray.size)
-        exoPlayer.prepare(*rendererArray)
+        exoPlayer = ExoPlayer.Factory.newInstance(rendererArray.size)
+        (exoPlayer as ExoPlayer?)?.prepare(*rendererArray)
+        (exoPlayer as ExoPlayer?)?.sendMessage(audioRenderer, MediaCodecAudioTrackRenderer.MSG_SET_PLAYBACK_PARAMS, null)
+        (exoPlayer as ExoPlayer?)?.playWhenReady = true
 
-        // ここでキー変えてる まじ大事なところ
-        val pitch = generatePitchFrequency(3)
-        exoPlayer.sendMessage(audioRenderer, MediaCodecAudioTrackRenderer.MSG_SET_PLAYBACK_PARAMS, PlaybackParams().setPitch(pitch))
-
-        exoPlayer.playWhenReady = true
+        val binding = DataBindingUtil.setContentView<PlayerActivityBinding>(this, R.layout.player_activity);
+        binding.pitchDown.setOnClickListener { sendChangePitchMessage(-1) }
+        binding.pitchUp.setOnClickListener { sendChangePitchMessage(1) }
     }
 
-    fun generatePitchFrequency(key: Int): Float {
+    fun sendChangePitchMessage(key: Int) {
+        // ここでキー変えてる まじ大事なところ
+        pitch += key
+        generatePitchFrequency(pitch)
+        val freq = generatePitchFrequency(pitch)
+        val playbackParam = PlaybackParams()
+        playbackParam.pitch = freq
+        playbackParam.speed = 1.0f // 毎回１をセットしないと、pitchに合わせて再生スピードが変わってしまう
+        exoPlayer?.sendMessage(audioRenderer, MediaCodecAudioTrackRenderer.MSG_SET_PLAYBACK_PARAMS, playbackParam)
+    }
+
+    fun generatePitchFrequency(key: Float): Float {
         return Math.pow(2.0, key.toDouble() / 12).toFloat()
     }
 
